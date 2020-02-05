360d68088d7b155e2bad4564ce86b3c069c2d079
yogesh paisode <yogesh@gg.com>
2020-02-05 21:46:31 +0530

    Added navigation

--- routes/map.js
+++ routes/map.js
@@ -3,7 +3,7 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('map', {
-        title: 'Yogesh'
+        title: 'Harvesting'
     });
 });
 module.exports = router;
